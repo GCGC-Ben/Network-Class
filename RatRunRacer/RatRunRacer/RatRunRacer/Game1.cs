315f2a57de3adf75b8ef7c1dbbc4c74122724d80
Jackson Whitley <jackson.whitley@bobcats.gcsu.edu>
2015-11-20 21:34:52 -0500

    Added working main menu and buttons as well as selection rats.

--- RatRunRacer/RatRunRacer/RatRunRacer/Game1.cs
+++ RatRunRacer/RatRunRacer/RatRunRacer/Game1.cs
@@ -14,6 +14,8 @@ namespace RatRunRacer
 
     public class Game1 : Microsoft.Xna.Framework.Game
     {
+        enum GameState {MMenu,playing,lobby,pause }
+        GameState state;
         GraphicsDeviceManager graphics;
         GraphicsDevice device;
         SpriteBatch spriteBatch;
@@ -21,6 +23,7 @@ namespace RatRunRacer
         Rat myPlayer;
         Camera2d cam;
         World World1;
+        MainMenu mainMenu;
 
         public Game1()
         {
@@ -31,13 +34,14 @@ namespace RatRunRacer
         protected override void Initialize()
         {
             device = graphics.GraphicsDevice;
-
+            state = GameState.MMenu;
             this.Window.Title = "Rat Run Racer";
             graphics.PreferredBackBufferWidth = 1280;//720p
             graphics.PreferredBackBufferHeight = 720;
             graphics.ApplyChanges();
 
             myPlayer = new Rat(Color.White, new Vector2(0, -10));
+            mainMenu = new MainMenu(new Vector2(640, 260), new Vector2(640, 360), new Vector2(640, 460));
 
             cam = new Camera2d();
             cam._pos.Y = -200;
@@ -50,7 +54,8 @@ namespace RatRunRacer
             spriteBatch = new SpriteBatch(GraphicsDevice);
             font = Content.Load<SpriteFont>("Fonts\\Font1");
             Rat.load(Content);
-
+            MainMenu.load(Content);
+            mainMenu.generateMenu();
             World.load(Content);
             World1 = new World();
         }
@@ -65,11 +70,30 @@ namespace RatRunRacer
            
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
                 this.Exit();
-
+            KeyboardState ks = Keyboard.GetState();
             myPlayer.update(World1);
+            mainMenu.Update();
             cam._pos.X = myPlayer.pos.X;
             World1.update();
 
+            //Selection switch case for main menu
+            if (state == GameState.MMenu && ks.IsKeyDown(Keys.Enter))
+            {
+                switch (mainMenu.getSelection())
+                {
+                    case 1:
+                        state = GameState.lobby;
+                        break;
+                    case 2:
+                        state = GameState.playing;
+                        break;
+                    case 3:
+                        state = GameState.pause;
+                        break;
+                    default:
+                        break;
+                }
+            }
 
             base.Update(gameTime);
         }
@@ -82,16 +106,21 @@ namespace RatRunRacer
 
             spriteBatch.Begin(SpriteSortMode.BackToFront, BlendState.AlphaBlend); //Start Hud draw
 
-            //spriteBatch.DrawString(font, "Jackson likes men a lot and he denies it", new Vector2(100, 100), new Color(255, 255, 255));
-
+            if (state == GameState.MMenu)
+            {
+                //spriteBatch.DrawString(font, "Jackson likes men a lot and he denies it", new Vector2(100, 100), new Color(255, 255, 255));
+                mainMenu.Draw(spriteBatch);
+            }
             spriteBatch.End();//end HUD draw
 
 
             spriteBatch.Begin(SpriteSortMode.BackToFront, BlendState.AlphaBlend,null,null,null,null,cam.get_transformation(device)); //Start Game Draw    TODO: add camera logic
 
-            myPlayer.draw(spriteBatch);
-            World1.draw(spriteBatch, cam.Pos);
-
+            if (state == GameState.playing)
+            {
+                myPlayer.draw(spriteBatch);
+                World1.draw(spriteBatch, cam.Pos);
+            }
             spriteBatch.End();//end game draw
 
 
--- RatRunRacer/RatRunRacer/RatRunRacer/MainMenu.cs
+++ RatRunRacer/RatRunRacer/RatRunRacer/MainMenu.cs
@@ -20,33 +20,45 @@ namespace RatRunRacer{
         String buttonResult;
 
         //tetures for buttons
-        Texture2D startTxt;
-        Texture2D optionsTxt;
-        Texture2D connectTxt;
+        static Texture2D startTxt;
+        static Texture2D optionsTxt;
+        static Texture2D connectTxt;
+        static Texture2D selectArrow;
         
         //Button positions
         Vector2 startPos;
         Vector2 optionsPos;
         Vector2 connectPos;
+        Vector2 arrowPos;
 
         //Button areas
         Rectangle startRec;
         Rectangle optionRec;
         Rectangle connectRec;
 
+        //select value
+        int selection;
+        int counter;
+        bool startWait;
+
         public MainMenu(Vector2 sPos, Vector2 oPos, Vector2 cPos){
             startPos = sPos;
             optionsPos = oPos;
             connectPos = cPos;
             buttonResult = "";
+            arrowPos = new Vector2(sPos.X,sPos.Y);
+            selection = 1;
+            counter = 30;
+            startWait = false;
         }
 
 
-        public void load(ContentManager content)
+        public static void load(ContentManager content)
         {
             startTxt = content.Load<Texture2D>("Player\\rat");
             optionsTxt = content.Load<Texture2D>("Player\\rat");
             connectTxt = content.Load<Texture2D>("Player\\rat");
+            selectArrow = content.Load<Texture2D>("Icons\\arrow");
         }
 
 
@@ -58,24 +70,78 @@ namespace RatRunRacer{
             main.addButton(connectTxt, connectPos, Color.Purple);
         }
 
+        //get the selection
+        public int getSelection()
+        {
+            return selection;
+        }
 
         //not sure what needs to bu updated with menu unless we want fancy stuff
         //also contains logic for commands
         public void Update()
         {
-            MouseState mState = Mouse.GetState();
-            /*
-            if (mState.LeftButton == ButtonState.Pressed)
-            {
-                if(mState.X.)
-            }
-            */
+            KeyboardState keyState = Keyboard.GetState();
+
+
+            if (keyState.IsKeyDown(Keys.Down) && selection <= 3 && startWait != true)
+                {
+
+                    selection++;
+
+                    switch (selection)
+                    {
+                        case 1:
+                            arrowPos.Y = startPos.Y;
+                            break;
+                        case 2:
+                            arrowPos.Y = optionsPos.Y;
+                            break;
+                        case 3:
+                            arrowPos.Y = connectPos.Y;
+                            break;
+                        default:
+                            break;
+                    }
+                    startWait = true;
+                }
+
+                if (keyState.IsKeyDown(Keys.Up) && selection >= 1 && startWait != true)
+                {
+                    selection--;
+                    switch (selection)
+                    {
+                        case 1:
+                            arrowPos.Y = startPos.Y;
+                            break;
+                        case 2:
+                            arrowPos.Y = optionsPos.Y;
+                            break;
+                        case 3:
+                            arrowPos.Y = connectPos.Y;
+                            break;
+                        default:
+                            break;
+                    }
+                    startWait = true;
+                }
+                if(startWait == true)
+                {
+                    counter--;
+                }
+                if (counter == 0)
+                {
+                    counter = 30;
+                    startWait = false;
+                }
+                
+          
         }
 
 
         public void Draw(SpriteBatch sb)
         {
             main.Draw(sb);
+            sb.Draw(selectArrow, arrowPos, null, Color.White, 0f, new Vector2(selectArrow.Width / 2, selectArrow.Height), 1f, SpriteEffects.None, .5f);
         }
 
     }
