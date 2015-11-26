1e515e7fdfbd0c645e530a176d5f554e999f604f
Jackson Whitley <jackson.whitley@bobcats.gcsu.edu>
2015-11-25 22:53:39 -0500

    Little bit of server stuff.

---
+++ Server/Server/Server/PlayerInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class PlayerInfo
+    {
+        String username;
+        int x;
+        int y;
+
+        // winning coords x:8224 y:1248
+        //dim: 320x160
+
+        public PlayerInfo()
+        {
+
+        }
+    }
+}
